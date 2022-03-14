7a413da9f27a2ec984d8a84b9ad15b2c70d8b171
Sudakks <100069699+Sudakks@users.noreply.github.com>
2022-03-14 21:07:34 +0800

    Modify my robots, now work well

--- first.cpp
+++ first.cpp
@@ -15,7 +15,7 @@ using namespace std;
 #define H_WALL 110//表示中间的硬墙数量
 #define S_WALL 145//表示中间软墙的个数
 #define Bomb_Wall 2//炸软墙2分
-#define Bomb_Robot 7//嗯现在还差捡道具没做了，加把劲写！！！(设定道具有使用时间，均为7s)
+#define Bomb_Robot 7//(设定道具有使用时间，均为7s)
 #define Bomb_player 10
 #define Game_time 540000//规定了游戏结束的时间9min
 const int ONE_SECOND = 5000;
@@ -27,9 +27,7 @@ enum director
 	RIGHT
 };
 void display();
-//
 int px1, py1, px2, py2;//这个每次储存的是player1和2当时炸弹爆炸的位置，这样爆炸的位置就不随着人的移动而移动
-int X1, Y1, X2, Y2, X3, Y3;//这个是储存机器人放炸弹时所在的位置
 class MAP
 {
 public:
@@ -98,7 +96,252 @@ void MAP::init_MYmap()
 		}
 	}
 }
-//
+class ROBOT
+{
+private:
+	pair<int, int> location;//现在的位置
+	char symbol;//机器人代表的字符
+	int wait_time = 0;
+	int bomb_time = 0;//是机器人的炸弹时间
+	pair<int, int> bomb_location;//这个是机器人当时放炸弹的地方
+	int alive = 1;//表示是否存活
+	int flag = 0;//是1的时候表示机器人刚刚放完炸弹，然后一定要先往一个方向移动，0就是已经移开区域了
+public:
+	int get_flag()
+	{
+		return this->flag;
+	}
+	void change_flag(int now)
+	{
+		flag = now;
+	}
+	pair<int, int> get_bomb_location()
+	{
+		return this->bomb_location;
+	}
+	void change_bomb_location(int x, int y)
+	{
+		this->bomb_location.first = x, this->bomb_location.second = y;
+	}
+	int get_wait_time()
+	{
+		return this->wait_time;
+	}
+	int get_bomb_time()
+	{
+		return this->bomb_time;
+	}
+	void change_wait_time(int x)
+	{
+		this->wait_time += x;
+	}
+	void change_bomb_time(int x)
+	{
+		this->bomb_time += x;
+	}
+	void init_robot(char character);
+	pair<int, int> get_location()//获取位置
+	{
+		return this->location;
+	}
+	char get_symbol()//获取机器人的字符表示
+	{
+		return this->symbol;
+	}
+	void change_location(int x, int y)
+	{
+		this->location.first = x, this->location.second = y;
+	}
+	void walk();
+	int get_alive()
+	{
+		return this->alive;
+	}
+	void change_alive(int now)
+	{
+		alive = now;
+	}
+};
+ROBOT robot1;
+ROBOT robot2;
+ROBOT robot3;
+void ROBOT::init_robot(char character)
+{
+	this->symbol = character;
+	while (1)
+	{
+		this->location.first = (rand() % row);
+		this->location.second = (rand() % col);
+		if (map.get_MYmap(this->location.first, this->location.second) == ' ')
+		{
+			map.change_MYmap(this->symbol, this->location.first, this->location.second);
+			break;
+		}
+	}
+}
+void ROBOT::walk()
+{
+	//利用随机数使三个机器人能够走动起来
+	char character = this->symbol;
+	int x = this->location.first, y = this->location.second;
+	int bomb_x = this->bomb_location.first, bomb_y = this->bomb_location.second;
+	map.change_MYmap(' ', x, y);
+	while (1)
+	{
+		if (map.get_MYmap(x - 1, y) != ' ' && map.get_MYmap(x + 1, y) != ' ' && map.get_MYmap(x, y - 1) != ' ' && map.get_MYmap(x, y + 1) != ' ')
+		{
+			return;
+		}
+		//原来是这种情况，一开始一定要先往一个方向走，不然就会死循环，而且还要走两步，所以特判空间！！！	
+		int direct = rand() % 4;
+		if (direct == UP && map.get_MYmap(x - 1, y) == ' ')
+		{
+			//分为两种情况，一种是没有炸弹，所以只要能走，机器人就会走
+			//第二种是有炸弹要判断是否会走到机器人所在的范围
+			if (flag == 1 || (!bomb_time && !wait_time))
+			{
+				this->location.first--;
+				x--, flag = 0;
+				map.change_MYmap(character, x, y);
+				display();
+				return;
+			}
+			else
+			{
+				//欸好像是循环出不去的问题，无语无语不知道咋改。。。
+				for (int i = bomb_x - 1; i <= bomb_x + 1; i++)
+				{
+					if (x - 1 == i && y == bomb_y)
+						//说明不能走
+						break;
+					if (i == bomb_x + 1)
+					{
+						for (int j = bomb_y - 1; j <= bomb_y + 1; j++)
+						{
+							if (x - 1 == bomb_x && y == j)
+								//说明不能走
+								break;
+							if (j == bomb_y + 1)
+							{
+								//这里是最终可以走的地方，然后开始改变
+								this->location.first--;
+								x--;
+								map.change_MYmap(character, x, y);
+								display();
+								return;//走完了直接返回main中	
+							}
+						}
+					}
+				}
+			}
+		}
+		else if (direct == DOWN && map.get_MYmap(x + 1, y) == ' ')
+		{
+			if (flag == 1 || (!wait_time && !bomb_time))
+			{
+				this->location.first++;
+				x++, flag = 0;
+				map.change_MYmap(character, x, y);
+				display();
+				break;
+			}
+			else
+			{
+				for (int i = bomb_x - 1; i <= bomb_x + 1; i++)
+				{
+					if (x + 1 == i && y == bomb_y)
+						break;
+					if (i == bomb_x + 1)
+					{
+						for (int j = bomb_y - 1; j <= bomb_y + 1; j++)
+						{
+							if (x + 1 == bomb_x && y == j)
+								break;
+							if (j == bomb_y + 1)
+							{
+								this->location.first++;
+								x++;
+								map.change_MYmap(character, x, y);
+								display();
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+		else if (direct == LEFT && map.get_MYmap(x, y - 1) == ' ')
+		{
+			if (flag == 1 || (!wait_time && !bomb_time))
+			{
+				this->location.second--;
+				y--, flag = 0;
+				map.change_MYmap(character, x, y);
+				display();
+				break;
+			}
+			else
+			{
+				for (int i = bomb_x - 1; i <= bomb_x + 1; i++)
+				{
+					if (x == i && y - 1 == bomb_y)
+						break;
+					if (i == bomb_x + 1)
+					{
+						for (int j = bomb_y - 1; j <= bomb_y + 1; j++)
+						{
+							if (x == bomb_x && y - 1 == j)
+								break;
+							if (j == bomb_y + 1)
+							{
+								this->location.second--;
+								y--;
+								map.change_MYmap(character, x, y);
+								display();
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+		else if (direct == RIGHT && map.get_MYmap(x, y + 1) == ' ')
+		{
+			if (flag == 1 || (!wait_time && !bomb_time))
+			{
+				this->location.second++;
+				y++, flag = 0;
+				map.change_MYmap(character, x, y);
+				display();
+				break;
+			}
+			else
+			{
+				for (int i = bomb_x - 1; i <= bomb_x + 1; i++)
+				{
+					if (x == i && y + 1 == bomb_y)
+						break;
+					if (i == bomb_x + 1)
+					{
+						for (int j = bomb_y - 1; j <= bomb_y + 1; j++)
+						{
+							if (x == bomb_x && y + 1 == j)
+								break;
+							if (j == bomb_y + 1)
+							{
+								this->location.second++;
+								y++;
+								map.change_MYmap(character, x, y);
+								display();
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
 class Player
 {
 private:
@@ -210,8 +453,8 @@ void Player::update_location(int x, int y)
 	//只是用来修改玩家的位置
 	this->location.first = x, this->location.second = y;
 }
-	//这里是模拟炸弹爆炸的过程
-	//包括产生光束，然后一秒钟恢复，判断有没有东西要被消除
+//这里是模拟炸弹爆炸的过程
+//包括产生光束，然后一秒钟恢复，判断有没有东西要被消除
 void process_of_explode(int x, int y, int range, int type)
 {
 	//相应的加分和处理游戏是否已经结束
@@ -222,6 +465,15 @@ void process_of_explode(int x, int y, int range, int type)
 		char now = map.get_MYmap(x, i);
 		if (now != '#')
 		{
+			if (now <= '9' && now >= '1')
+			{
+				if (now == '1')
+					robot1.change_alive(0);
+				else if (now == '2')
+					robot2.change_alive(0);
+				else
+					robot3.change_alive(0);
+			}
 			if (type == 1)
 			{
 				//说明是1号玩家的操作
@@ -246,8 +498,10 @@ void process_of_explode(int x, int y, int range, int type)
 				}
 				else if (now == '@')
 					player1.change_alive(0);//自己炸自己
-				else if(now <= '9'&& now >= '1')
+				else if (now <= '9' && now >= '1')
+				{
 					player1.change_score(Bomb_Robot);
+				}				
 			}
 			else if(type == 2)
 			{
@@ -274,19 +528,9 @@ void process_of_explode(int x, int y, int range, int type)
 				else if (now == '+')
 					player2.change_alive(0);
 				else if (now <= '9' && now >= '1')
+				{
 					player2.change_score(Bomb_Robot);
-			}
-			else
-			{
-				//这里是机器人放炸弹的操作
-				if (now == '@')
-				{
-					player1.change_alive(0);
-				}
-				else if (now == '+')
-				{
-					player2.change_alive(0);
-				}
+				}			
 			}
 			map.change_MYmap('O', x, i);
 		}
@@ -297,6 +541,16 @@ void process_of_explode(int x, int y, int range, int type)
 		char now = map.get_MYmap(i, y);
 		if (now != '#')
 		{
+			if (now <= '9' && now >= '1')
+			{
+				//就是不管对哪一个玩家而言都是一样的操作
+				if (now == '1')
+					robot1.change_alive(0);
+				else if (now == '2')
+					robot2.change_alive(0);
+				else
+					robot3.change_alive(0);
+			}
 			if (type == 1)
 			{
 				//说明是1号玩家的操作
@@ -322,7 +576,9 @@ void process_of_explode(int x, int y, int range, int type)
 				else if (now == '@')
 					player1.change_alive(0);
 				else if (now <= '9' && now >= '1')
+				{
 					player1.change_score(Bomb_Robot);
+				}				
 			}
 			else if (type == 2)
 			{
@@ -349,28 +605,18 @@ void process_of_explode(int x, int y, int range, int type)
 				else if (now == '+')
 					player2.change_alive(0); 
 				else if (now <= '9' && now >= '1')
+				{
 					player2.change_score(Bomb_Robot);
-			}
-			else
-			{
-				//这里是机器人放炸弹的操作
-				if (now == '@')
-				{
-					player1.change_alive(0);
 				}
-				else if (now == '+')
-				{
-					player2.change_alive(0);
-				}
+					
 			}
 			map.change_MYmap('O', i, y);
 		}
 	}//竖条
-	display();//然后先显示
-}//既可以玩家用，也可以机器人用
+	display();
+}
 void process_of_show(int x, int y, int range)
 {
-	char ord = map.get_MYmap(x, y);//获取玩家的符号
 	//等光束展示了1s后，即恢复到原来的效果，应该是能发出光线的地方都被变成了空地
 	for (int i = x - range; i <= x + range; i++)
 	{
@@ -383,133 +629,6 @@ void process_of_show(int x, int y, int range)
 		}
 	}
 	display();
-}
-class ROBOT
-{
-private:
-	pair<int, int> location;//现在的位置
-	char symbol;//机器人代表的字符
-	int wait_time = 0;
-	int bomb_time = 0;//是机器人的炸弹时间
-public:
-	int get_wait_time()
-	{
-		return this->wait_time;
-	}
-	int get_bomb_time()
-	{
-		return this->bomb_time;
-	}
-	void change_wait_time(int x)
-	{
-		this->wait_time += x;
-	}
-	void change_bomb_time(int x)
-	{
-		this->bomb_time += x;
-	}
-	void init_robot(char character);
-	pair<int, int> get_location()//获取位置
-	{
-		return this->location;
-	}
-	char get_symbol()//获取机器人的字符表示
-	{
-		return this->symbol;
-	}
-	void change_location(int x, int y)
-	{
-		this->location.first = x, this->location.second = y;
-	}
-	void walk();
-};
-ROBOT robot1;
-ROBOT robot2;
-ROBOT robot3;
-void ROBOT::init_robot(char character)
-{
-	this->symbol = character;
-	while (1)
-	{
-		this->location.first = (rand() % row);
-		this->location.second = (rand() % col);
-		if (map.get_MYmap(this->location.first, this->location.second) == ' ')
-		{
-			map.change_MYmap(this->symbol, this->location.first, this->location.second);
-			break;
-		}
-	}
-}
-void ROBOT::walk()
-{
-	//利用随机数使三个机器人能够走动起来
-	char character = this->symbol;
-	int x = this->get_location().first;
-	int y = this->get_location().second;
-	map.change_MYmap(' ', x, y);
-	while (1)
-	{
-		int direct = rand() % 4;
-		if (direct == UP && map.get_MYmap(x - 1, y) == ' ')
-		{
-			this->location.first--;			
-			x--;
-			map.change_MYmap(character, x, y);
-			display();
-			break;
-		}
-		else if (direct == DOWN && map.get_MYmap(x + 1, y) == ' ')
-		{
-			this->location.first++;
-			x++;
-			map.change_MYmap(character, x, y);
-			display();
-			break;
-		}
-		else if (direct == LEFT && map.get_MYmap(x, y - 1) == ' ')
-		{
-			this->location.second--;
-			y--;
-			map.change_MYmap(character, x, y);
-			display();
-			break;
-		}
-		else if (direct == RIGHT && map.get_MYmap(x, y + 1) == ' ')
-		{
-			this->location.second++;
-			y++;
-			map.change_MYmap(character, x, y);
-			display();
-			break;
-		}
-	}
-	//不使用随机数，若使用一直朝一个方向走动，顺序上左下右
-	/*int x = this->get_location().first, y = this->get_location().second;
-	if (map.get_MYmap(x - 1, y) == ' ')
-	{
-		map.change_MYmap(' ', x, y);
-		map.change_MYmap(this->symbol, x - 1, y);
-		this->change_location(x - 1, y);
-	}
-	else if (map.get_MYmap(x, y - 1) == ' ')
-	{
-		map.change_MYmap(' ', x, y);
-		map.change_MYmap(this->symbol, x, y - 1);
-		this->change_location(x, y - 1);
-	}
-	else if (map.get_MYmap(x + 1, y) == ' ')
-	{
-		map.change_MYmap(' ', x, y);
-		map.change_MYmap(this->symbol, x + 1, y);
-		this->change_location(x + 1, y);
-	}
-	else if (map.get_MYmap(x, y + 1) == ' ')
-	{
-		map.change_MYmap(' ', x, y);
-		map.change_MYmap(this->symbol, x, y + 1);
-		this->change_location(x, y + 1);
-	}
-	display();*/
 }
 void init()
 {
@@ -607,7 +726,7 @@ void deal_with_input()
 				{
 					Px1++;
 				}
-			}//利用Px1和Py1分别代表现在玩家应该存在的地方
+			}
 			map.change_MYmap(' ', player1.get_location().first, player1.get_location().second);//将现在的位置变为空
 			player1.update_location(Px1, Py1);
 			map.change_MYmap(player1.get_symbol(), Px1, Py1);//显示玩家现在的位置
@@ -696,12 +815,13 @@ void deal_with_timer()
 {
 	//...怎么会有这么多重复的复杂的代码就是说
 	//定时事件：炸弹定时3s爆炸，光束显示1s
+	//printf("机器人的爆炸时间为%d,炸弹等待时间为%d\n", robot1.get_bomb_time(), robot1.get_wait_time());
 	if (robot1.get_bomb_time())
 	{
 		robot1.change_bomb_time(-1 * ONE_SECOND);
 		if (robot1.get_bomb_time() == 0)//说明此时要爆炸了
 		{
-			process_of_show(X1, Y1, 1);
+			process_of_show(robot1.get_bomb_location().first, robot1.get_bomb_location().second, 1);
 		}
 	}
 	if (robot2.get_bomb_time())
@@ -709,7 +829,7 @@ void deal_with_timer()
 		robot2.change_bomb_time(-1 * ONE_SECOND);
 		if (robot2.get_bomb_time() == 0)//说明此时要爆炸了
 		{
-			process_of_show(X2, Y2, 1);
+			process_of_show(robot2.get_bomb_location().first, robot2.get_bomb_location().second, 1);
 		}
 	}
 	if (robot3.get_bomb_time())
@@ -717,24 +837,26 @@ void deal_with_timer()
 		robot3.change_bomb_time(-1 * ONE_SECOND);
 		if (robot3.get_bomb_time() == 0)//说明此时要爆炸了
 		{
-			process_of_show(X3, Y3, 1);
+			process_of_show(robot3.get_bomb_location().first, robot3.get_bomb_location().second, 1);
 		}
 	}
 	if (robot1.get_wait_time())
 	{
 		robot1.change_wait_time(-1 * ONE_SECOND);
+		
 		if (robot1.get_wait_time() == 0)
 		{
-			process_of_explode(X1, Y1, 1, 0);
+			process_of_explode(robot1.get_bomb_location().first, robot1.get_bomb_location().second, 1, 0);
 			robot1.change_bomb_time(1 * ONE_SECOND);
 		}
+		//printf("机器人的爆炸时间为%d,炸弹等待时间为%d\n", robot1.get_bomb_time(), robot1.get_wait_time());
 	}
 	if (robot2.get_wait_time())
 	{
 		robot2.change_wait_time(-1 * ONE_SECOND);
 		if (robot2.get_wait_time() == 0)
 		{
-			process_of_explode(X2, Y2, 1, 0);
+			process_of_explode(robot2.get_bomb_location().first, robot2.get_bomb_location().second, 1, 0);
 			robot2.change_bomb_time(1 * ONE_SECOND);
 		}
 	}
@@ -743,7 +865,7 @@ void deal_with_timer()
 		robot3.change_wait_time(-1 * ONE_SECOND);
 		if (robot3.get_wait_time() == 0)
 		{
-			process_of_explode(X3, Y3, 1, 0);
+			process_of_explode(robot3.get_bomb_location().first, robot3.get_bomb_location().second, 1, 0);
 			robot3.change_bomb_time(1 * ONE_SECOND);
 		}
 	}
@@ -820,37 +942,44 @@ int main()
 	srand((unsigned)time(NULL));//随机数种子
 	init();//作为初始化地图等东西
 	map.show_MYmap();
-	long long cnt = 1;
-	long long start_t = clock();
-	long long end_t;
+	long long cnt = 0, start_t = clock(), end_t;
 	while (1)
 	{
 		deal_with_input();
 		cnt++;
-		if (cnt % ONE_SECOND == 0)
+		if (cnt == ONE_SECOND)
 		{
-
 			deal_with_timer();
-
-		}
-		if (cnt == 2 * ONE_SECOND)
-		{
-			robot1.walk();
-			robot2.walk();
-			robot3.walk();
-		}
-		if (cnt == 5 * ONE_SECOND)
-		{
-			cnt = 1;
-			robot1.change_wait_time(3 * ONE_SECOND);
-			X1 = robot1.get_location().first;
-			Y1 = robot1.get_location().second;
-			robot2.change_wait_time(3 * ONE_SECOND);
-			X2 = robot2.get_location().first;
-			Y2 = robot2.get_location().second;
-			robot3.change_wait_time(3 * ONE_SECOND);
-			X3 = robot3.get_location().first;
-			Y3 = robot3.get_location().second;
+			cnt = 0;
+			//要先判断机器人是否还存在，若不存在，就不能让他继续走
+			if (robot1.get_alive())
+				robot1.walk();//所以是walk写的有问题。。。真的找了好久，太绝望了
+			if (robot2.get_alive())
+				robot2.walk();
+			if (robot3.get_alive())
+				robot3.walk();
+			int random = rand() % 5;
+			if (random == 1 && robot1.get_alive() && !robot1.get_bomb_time() && !robot1.get_wait_time())
+			{
+				printf("机器人1释放炸弹，小心！\n");
+				robot1.change_flag(1);
+				robot1.change_bomb_location(robot1.get_location().first, robot1.get_location().second);
+				robot1.change_wait_time(3 * ONE_SECOND);
+			}
+			if (random == 2 && robot2.get_alive() && !robot2.get_bomb_time() && !robot2.get_wait_time())
+			{
+				printf("机器人2释放炸弹，小心！\n");
+				robot2.change_flag(1);
+				robot2.change_bomb_location(robot2.get_location().first, robot2.get_location().second);
+				robot2.change_wait_time(3 * ONE_SECOND);
+			}
+			if (random == 3 && robot3.get_alive() && !robot3.get_bomb_time() && !robot3.get_wait_time())
+			{
+				printf("机器人3释放炸弹，小心！\n");
+				robot3.change_flag(1);
+				robot3.change_bomb_location(robot3.get_location().first, robot3.get_location().second);
+				robot3.change_wait_time(3 * ONE_SECOND);
+			}
 		}
 		end_t = clock();
 		if (end_t - start_t == Game_time)
